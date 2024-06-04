e39cba5e2e31860ac3dbd136d3c5a1a9be177650
Giovanni Giorgi <daitangio@users.noreply.github.com>
2024-06-04 16:31:26 +0000

    New codespace with python 11

--- bin/installBasicChat.sh
+++ bin/installBasicChat.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-for m in  gemma:2b phi3 llama3:8b mistral:7b mistral-openorca:7b ; do
+# llama3:8b mistral:7b mistral-openorca:7b
+for m in  gemma:2b phi3  ; do
     echo Pulling $m
     docker exec -it ollama ollama pull $m
 done
